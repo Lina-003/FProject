f84727a1eb85a3083d6d06aec735cd699067e612
CaroV2 <carovel2608@gmail.com>
2023-09-22 02:28:18 -0500

    feat: home components created

---
+++ src/components/home/home.ts
@@ -0,0 +1,70 @@
+export enum Attribute2 {
+  "img" = "img",
+  "title" = "title",
+  "spot" = "spot",
+}
+
+class Home extends HTMLElement {
+  img?: string;
+  customTitle?: string;
+  spot?: string;
+  propName: string | undefined;
+
+  static get observedAttributes() {
+    return [Attribute2.img, Attribute2.title, Attribute2.spot];
+  }
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  attributeChangedCallback(
+    propName: Attribute2,
+    _: string | undefined,
+    newValue: string | undefined
+  ) {
+    switch (propName) {
+      case Attribute2.img:
+        this.img = newValue ? String(newValue) : undefined;
+        break;
+      case Attribute2.title:
+        this.customTitle = newValue ? String(newValue) : undefined;
+        break;
+      case Attribute2.spot:
+        this.spot = newValue ? String(newValue) : undefined;
+
+      default:
+        this.propName = newValue;
+        break;
+    }
+
+    this.render();
+  }
+
+  render() {
+    const searchImg = this.ownerDocument.createElement("img");
+    if (this.shadowRoot) {
+      this.shadowRoot.innerHTML = `
+          <link rel="stylesheet" href="../src/components/home/home.css">
+          
+          <section>
+          <div class="images">
+          <img src=${this.img} height="267.57px" weight="200.68px" alt="">
+          </div>
+
+          <div class="text">
+          <h1>${this.title}</h1>
+          <h3>${this.spot}</h3>
+          </div>
+          </section>
+          `;
+    }
+  }
+}
+customElements.define("app-home", Home);
+export default Home;
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,64 @@
 import Header from "./components/header/Header";
-import { Attribute } from "./components/header/Header"; // Asegúrate de importar Attribute desde tu Header
+import Home from "./components/home/Home";
+import { Attribute } from "./components/header/Header";
+import { Attribute2 } from "./components/home/Home";
 import { data } from "./dataHeader";
+import { tSpot } from "./dataSpot";
 
 class AppContainer extends HTMLElement {
-    header: Header[] = [];
+  header: Header[] = [];
+  home: Home[] = [];
 
-    constructor() {
-        super();
-        this.attachShadow({ mode: "open" });
-        
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
 
-            data.forEach((menu) => {
-           const menuHeader = this.ownerDocument.createElement("app-header") as Header;
-            // Configura los atributos de Header si es necesario
-            menuHeader.setAttribute(Attribute.logo, menu.logo);
-            menuHeader.setAttribute(Attribute.regions, menu.regions);
-            menuHeader.setAttribute(Attribute.climate, menu.climate);
-            menuHeader.setAttribute(Attribute.search, menu.search);
-            menuHeader.setAttribute(Attribute.profile, menu.profile);
-            // Agrega Header como hijo de AppContainer
-            this.header.push(menuHeader)
-            // Agrega más contenido o configuración específica de AppContainer aquí
-        })
-    }
+    data.forEach((menu) => {
+      const menuHeader = this.ownerDocument.createElement(
+        "app-header"
+      ) as Header;
+      // Configura los atributos de Header si es necesario
+      menuHeader.setAttribute(Attribute.logo, menu.logo);
+      menuHeader.setAttribute(Attribute.regions, menu.regions);
+      menuHeader.setAttribute(Attribute.climate, menu.climate);
+      menuHeader.setAttribute(Attribute.search, menu.search);
+      menuHeader.setAttribute(Attribute.profile, menu.profile);
+      // Agrega Header como hijo de AppContainer
+      this.header.push(menuHeader);
+      // Agrega más contenido o configuración específica de AppContainer aquí
+    });
 
-    connectedCallback() {
-        this.render();
-    }
+    tSpot.forEach((spot) => {
+      const homeSpot = this.ownerDocument.createElement("app-home") as Home;
+      // Configura los atributos de Header si es necesario
+      homeSpot.setAttribute(Attribute2.img, spot.img);
+      homeSpot.setAttribute(Attribute2.title, spot.title);
+      homeSpot.setAttribute(Attribute2.spot, spot.spot);
+      // Agrega Header como hijo de AppContainer
+      this.home.push(homeSpot);
+      // Agrega más contenido o configuración específica de AppContainer aquí
+    });
+  }
 
-    render() {
-        if (this.shadowRoot) {
-            this.shadowRoot.innerHTML = `
-           
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    if (this.shadowRoot) {
+      this.shadowRoot.innerHTML = `
+            <link href="dist/style.css" rel="stylesheet" />
             `;
 
-            this.header.forEach((nav) => {
-                this.shadowRoot?.appendChild(nav);
-            })
-        }
+      this.header.forEach((nav) => {
+        this.shadowRoot?.appendChild(nav);
+      });
+
+      this.home.forEach((nav) => {
+        this.shadowRoot?.appendChild(nav);
+      });
     }
+  }
 }
 
 customElements.define("app-container", AppContainer);
