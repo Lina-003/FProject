12eefb012ea94c476fb46c6764ee283af166d8e2
Carolina <carovel2608@gmail.com>
2023-11-29 21:14:21 -0500

    feat: Save and get add to actions

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,4 +1,7 @@
 import { Screens } from "../types/navigation";
+import { Product } from "../types/products";
+import { Actions, someActions } from "../types/store";
+import firebase from "../utils/firebase/firebase";
 
 export const navigate = (screen: Screens) => {
     return {
@@ -6,3 +9,19 @@ export const navigate = (screen: Screens) => {
         payload: screen
     }
 };
+
+export const saveProduct = async (product: Product): Promise<Actions> => {
+    await firebase.saveProductInDB(product);
+    return {
+      action: someActions.SAVE_COMMENT,
+      payload: product,
+    };
+  };
+  
+  export const getProducts = async (): Promise<Actions> => {
+    const products = await firebase.getProductsFromDB();
+    return {
+      action: someActions.GET_COMMENT,
+      payload: products,
+    };
+  };
