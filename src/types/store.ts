b8014abd2cb5e3fe41063b3a23c613abfceff23d
Carolina <carovel2608@gmail.com>
2023-11-29 21:16:15 -0500

    feat: Save and get add to store

--- src/types/store.ts
+++ src/types/store.ts
@@ -1,3 +1,5 @@
+import { Product } from "./products";
+
 export type Observer = ({ render: () => void } & HTMLElement);
 
 export type AppState = {
@@ -8,4 +10,19 @@ export enum ScreenActions {
   "NAVIGATE" = "NAVIGATE",
 }
 
-export type Actions = ScreenActions;
+export enum someActions {
+  "SAVE_COMMENT" = "SAVE_COMMENT",
+  "GET_COMMENT" = "GET_COMMENT",
+}
+
+export interface saveProductAction {
+  action: someActions.SAVE_COMMENT;
+  payload: Product,  
+}
+
+export interface getProductAction {
+  action: someActions.GET_COMMENT;
+  payload: Product[],  
+}
+
+export type Actions = ScreenActions | saveProductAction | getProductAction;
