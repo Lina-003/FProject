ce10b844f3626c1e36e6aa7623f2de70ebf85c93
Carolina <carovel2608@gmail.com>
2023-11-29 21:13:04 -0500

    feat: created comment

---
+++ src/components/Comments/Comment.ts
@@ -0,0 +1,40 @@
+import { dispatch } from "../../store";
+import { saveProduct } from "../../store/actions";
+import { Product } from "../../types/products"
+
+const userInput: Product = {
+    comm: ""
+}
+
+class Comment extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({mode:"open"})
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        if(this.shadowRoot) this.shadowRoot.innerHTML = ""
+
+        const comm = this.ownerDocument.createElement("textarea");
+        comm.addEventListener("change", (e: any) => {
+          userInput.comm = e.target.value;
+        })
+
+        const btn = this.ownerDocument.createElement("button");
+        btn.textContent = "Publicar";
+        btn.addEventListener("click", async () => {
+            console.log(userInput);
+            dispatch(await saveProduct(userInput))
+        })
+
+        this.shadowRoot?.appendChild(comm);
+        this.shadowRoot?.appendChild(btn);
+    }
+}
+
+export default Comment;
+customElements.define("app-comment", Comment);
