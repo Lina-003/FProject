e1ed950efe512e78f683c438b7cf650cd3daa9fe
Carolina <carovel2608@gmail.com>
2023-11-29 21:12:38 -0500

    feat: export comment in index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Header } from "./Header/header";
 export {default as Home} from "./Home/home";
 export { default as Recommended } from "./recommended/Recommended";
 export { default as SelectedSpot} from "./selectedspot/Selectedspot"
+export { default as Comment } from "./Comments/Comment";
