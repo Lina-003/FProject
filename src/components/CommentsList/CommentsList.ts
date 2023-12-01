055338d59707b2f37cd827331c9e1a8fd21706f3
Carolina <carovel2608@gmail.com>
2023-12-01 12:52:33 -0500

    feat: commentsList styles

--- src/components/CommentsList/CommentsList.ts
+++ src/components/CommentsList/CommentsList.ts
@@ -1,34 +1,41 @@
 import { addObserver, appState } from "../../store";
-import { loadCss } from '../../utils/styles';
-import style from './CommentsList.css';
+import { loadCss } from "../../utils/styles";
+import style from "./CommentsList.css";
 
 class CommentsList extends HTMLElement {
-    constructor(){
-        super();
-        this.attachShadow({mode: "open"})
-        addObserver(this);
-    }
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    addObserver(this);
+  }
 
-    connectedCallback() {
-        this.render()
-    }
+  connectedCallback() {
+    this.render();
+  }
 
-    render() {
-        if (this.shadowRoot) this.shadowRoot.innerHTML = "";
-        loadCss(this, style);
-        appState.products.forEach((p) => {
-            const containerComList = this.ownerDocument.createElement("section");
-            const cContainer = this.ownerDocument.createElement('article');
-            const cText = this.ownerDocument.createElement('h3');
-            cText.textContent = p.comm;
+  render() {
+    if (this.shadowRoot) this.shadowRoot.innerHTML = "";
+    loadCss(this, style);
+    appState.products.forEach((p) => {
+      const imgProfile = this.ownerDocument.createElement("img");
+      imgProfile.classList.add("class-profile");
+      imgProfile.setAttribute("src", "/src/components/imgheader/profile.png");
+      imgProfile.setAttribute("height", "35np");
 
-            cContainer?.appendChild(cText);
-            this.shadowRoot?.appendChild(cContainer);      
-            this.shadowRoot?.appendChild(containerComList);      
-        })
-    
-    }
+      const contentText = this.ownerDocument.createElement("div")
+      contentText.classList.add("content-text")
+
+      const cContainer = this.ownerDocument.createElement("article");
+      const cText = this.ownerDocument.createElement("p");
+      cText.textContent = p.comm;
+
+      cContainer?.appendChild(imgProfile)
+      cContainer?.appendChild(contentText);
+      contentText?.appendChild(cText);
+      this.shadowRoot?.appendChild(cContainer);
+    });
+  }
 }
 
-customElements.define('comment-list', CommentsList);
+customElements.define("comment-list", CommentsList);
 export default CommentsList;
