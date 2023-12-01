71381170624d31451c18f6611b6eee8f792ef761
Carolina <carovel2608@gmail.com>
2023-11-30 23:00:03 -0500

    add: Attribute2.obj and obj

--- src/components/home/home.ts
+++ src/components/home/home.ts
@@ -1,25 +1,27 @@
 import { dispatch } from "../../store";
-import { navigate } from "../../store/actions";
+import { navigate, selectSpot } from "../../store/actions";
 import { Screens } from "../../types/navigation";
-import { loadCss } from '../../utils/styles';
-import styles from './home.css';
+import { Spot } from "../../types/spot";
+import { loadCss } from "../../utils/styles";
+import styles from "./home.css";
 
 export enum Attribute2 {
   "img" = "img",
   "title" = "title",
   "spot" = "spot",
   "region" = "region",
-  "climate" = "climate"
+  "climate" = "climate",
+  "obj" = "obj",
 }
 
 class Home extends HTMLElement {
   img?: string;
   customTitle?: string;
   spot?: string;
+  obj?: string;
   propName: string | undefined;
-
   static get observedAttributes() {
-    return [Attribute2.img, Attribute2.title, Attribute2.spot];
+    return [Attribute2.img, Attribute2.title, Attribute2.spot, Attribute2.obj];
   }
 
   constructor() {
@@ -46,43 +48,47 @@ class Home extends HTMLElement {
       case Attribute2.spot:
         this.spot = newValue ? String(newValue) : undefined;
 
+      case Attribute2.obj:
+        this.obj = newValue ? String(newValue) : undefined;
+        break;
+
       default:
         this.propName = newValue;
         break;
     }
-
   }
 
   render() {
-
     if (this.shadowRoot) this.shadowRoot.innerHTML = "";
-      loadCss(this, styles)
+    loadCss(this, styles);
 
+    const container = this.ownerDocument.createElement("section");
+    container.classList.add("class-styles");
 
-      const container = this.ownerDocument.createElement("section");
-      container.classList.add("class-styles");
-      
-      const imgContent = this.ownerDocument.createElement("img");
-      imgContent.classList.add("class-image");
-      imgContent.setAttribute("src", `${this.img}`);
-      imgContent.setAttribute("height", "267.57px");
-      imgContent.setAttribute("width", "200.68px");
-      imgContent.addEventListener("click", () => {
-        dispatch(navigate(Screens.SPOTSELECT))
-      })
-      
-      const divText = this.ownerDocument.createElement("h1");
-      divText.classList.add("tittle-spot");
-      divText.innerText = `${this.title}`;
+    const imgContent = this.ownerDocument.createElement("img");
+    imgContent.classList.add("class-image");
+    imgContent.setAttribute("src", `${this.img}`);
+    imgContent.setAttribute("height", "267.57px");
+    imgContent.setAttribute("width", "200.68px");
+    imgContent.addEventListener("click", () => {
+      if (this.obj) {
+        let newObj = JSON.parse(this.obj) as Spot;
+        dispatch(selectSpot(newObj));
+        dispatch(navigate(Screens.SPOTSELECT));
+      }
+    });
 
-      const textSpot = this.ownerDocument.createElement("h3");
-      textSpot.innerText = `${this.spot}`;
+    const divText = this.ownerDocument.createElement("h1");
+    divText.classList.add("tittle-spot");
+    divText.innerText = `${this.title}`;
 
-  
-      this.shadowRoot?.appendChild(imgContent);
-      this.shadowRoot?.appendChild(divText);
-      this.shadowRoot?.appendChild(textSpot);
-      this.shadowRoot?.appendChild(container);
+    const textSpot = this.ownerDocument.createElement("h3");
+    textSpot.innerText = `${this.spot}`;
+
+    this.shadowRoot?.appendChild(imgContent);
+    this.shadowRoot?.appendChild(divText);
+    this.shadowRoot?.appendChild(textSpot);
+    this.shadowRoot?.appendChild(container);
   }
 }
 customElements.define("app-home", Home);
